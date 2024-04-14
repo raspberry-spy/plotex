f05acfaf0a372225eddc402da389f264a02dffd6
nit <wisemisticaltree5@gmail.com>
2024-04-14 17:39:27 +0300

    a

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ ax.plot(sec,
          228.409, 225.09, 219.573, 210.293, 199.481, 185.572, 170.088, 151.667, 133.509, 112.371, 87.9137, 65.6869,
          40.9764, 15.7691, 0], color='red')  # Высота
 ax.plot([-1, 15], [0, 0], color='black')  # Нулевая линия
+ax.plot(sec, [16, 15.0598, 15.0342, 15.0088, 14.9984, 14.9883, 14.9832, 14.9781, 14.9781, 14.9729, 14.9678, 14.9627,
+              14.9525, 14.9473, 14.9422, 14.932, 14.9166, 14.9219, 14.9064, 14.9064, 14.8912, 14.8656, 14.8656,
+              14.8451, 14.8145, 14.8145, 14.8043, 14.8043], color='orange')  # Температура
 ax.plot(sec,
         [0, 50, 60, 64, 66, 57, 44, 41, 28, 20, 16, 8, 5, -1, -6, -11, -18, -21, -27, -30, -36, -36, -42, -48, -44,
          -49, -50, 0], color='green')  # Скорость
@@ -23,7 +26,7 @@ plt.vlines(x=6, ymin=-60, ymax=250, color='black', linestyles="dashed")
 plt.text(x=6, y=150, s="Апогей", ha='center', va='center', rotation='vertical', backgroundcolor='white')
 
 plt.xlabel('Время (секунды)')
-plt.legend(['Высота', 'Нулевая линия', 'Скорость', 'Ускорение'], loc="upper left")
+plt.legend(['Высота', 'Нулевая линия', 'Температура', 'Скорость', 'Ускорение'], loc="upper left")
 ax.xaxis.set_major_locator(plticker.MultipleLocator(base=0.5))
 ax.yaxis.set_major_locator(plticker.MultipleLocator(base=10))
 plt.show()
